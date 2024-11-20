96492b55b5f16f28bb5f6034b6b7cee29dfbab05
Syed Sayeeduddin <iamsayeeduddin@outlook.com>
2024-11-20 20:00:23 +0530

    Req/Res Controller

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 import http from "http";
 // const require = require("http");
 
-const server = http.createServer();
+// const controller = ;
+
+const server = http.createServer((req, res) => {
+  //   console.log(req.body);
+  //   res.statusCode = 400;
+  //   res.end("Hello World!");
+
+  if (req.url.includes("welcome")) {
+    res.statusCode = 200;
+    res.end("Welcome to My Backend Server!");
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
+});
 
 server.listen(5000, () => console.log("Server UP & Running!"));
